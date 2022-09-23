98f43a808ecef77b95b5da2362d3deacb380768d
tjohns96 <trevorjohnson9601@gmail.com>
2022-09-23 11:31:10 -0400

    Add open and close menu functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+const navMenu = document.querySelector("#nav-menu");
+const navToggle = document.querySelector("#nav-toggle");
+const navClose = document.querySelector("#nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
